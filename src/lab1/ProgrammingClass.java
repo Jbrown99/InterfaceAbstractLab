cab5ee9ebd284a55e75c524c313112f766c884e2
joshuabrown <joshuabrown@10.30.21.6>
2015-09-10 17:39:30 -0500

    Made some slight changes to all classes, private variables, methods final

---
+++ src/lab1/ProgrammingClass.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab1;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author jbrown99
+ */
+public abstract class ProgrammingClass {
+    
+    private String courseName;
+    private String courseNumber;
+    private double credits;
+    
+      public ProgrammingClass(String courseName, String courseNumber) {
+        this.setCourseName(courseName);
+        this.setCourseNumber(courseNumber);
+    }
+    
+    public final String getCourseName() {
+        return courseName;
+    }
+
+    public final void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public final String getCourseNumber() {
+        return courseNumber;
+    }
+
+    public final void setCourseNumber(String courseNumber) {
+        if(courseNumber == null || courseNumber.length() == 0) {
+            JOptionPane.showMessageDialog(null,
+                    "Error: courseNumber cannot be null of empty string");
+            System.exit(0);
+        }
+        this.courseNumber = courseNumber;
+    }
+    
+     public final double getCredits() {
+        return credits;
+    }
+     
+     public abstract void setCredits(double credits);
+
+}
